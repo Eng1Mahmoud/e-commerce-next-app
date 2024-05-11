2b234cf87be63cc3c4142f64dcaba90823a22567
Mahmoud Mohamed <eslam.mohamed@fci.sohag.edu.eg>
2024-05-11 22:44:07 +0300

    " "

--- src/app/(main-layout)/layout.tsx
+++ src/app/(main-layout)/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" dir="rtl">
-      <body  suppressHydrationWarning>
+      <body >
           <Navbar/>
         {children}
         <footer>
