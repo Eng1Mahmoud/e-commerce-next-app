3676908f4ea3a36a6f328621347b910a386b976f
Mahmoud Mohamed <eslam.mohamed@fci.sohag.edu.eg>
2024-05-11 22:39:37 +0300

    remove font

--- src/app/(main-layout)/layout.tsx
+++ src/app/(main-layout)/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import ".././globals.css";
 import Navbar from "@/components/Header/Navbar";
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" dir="rtl">
-      <body className={inter.className} suppressHydrationWarning>
+      <body  suppressHydrationWarning>
           <Navbar/>
         {children}
         <footer>
