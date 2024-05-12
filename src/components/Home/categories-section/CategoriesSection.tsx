d8eada52bd5892b40918cb86c064ce7cb29ef14c
Mahmoud Mohamed <eslam.mohamed@fci.sohag.edu.eg>
2024-05-12 22:14:07 +0300

    update file to client component

--- src/components/Home/categories-section/CategoriesSection.tsx
+++ src/components/Home/categories-section/CategoriesSection.tsx
@@ -1,25 +1,30 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { SliderProducts } from "./SliderProducts";
 
-export const CategoriesSection = async ({
-  categorie,
-}: {
-  categorie: string;
-}) => {
-  console.log("categorie: ", categorie);
-  // get all vegetable products
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/products/${categorie}/${1}/${10}`,
-    {
-      method: "GET",
-    }
-  );
-  const data = await response.json();
-  console.log("data: ", data);
+export const CategoriesSection = ({ categorie }: { categorie: string }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/products/${categorie}/${1}/${10}`,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.products);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [categorie]);
+
   return (
     <div className="container mt-10">
       <h2 className="font-bold text-[25px]">{categorie}</h2>
-      <SliderProducts products={data} categorie={categorie} />
+      <SliderProducts products={products} categorie={categorie} />
     </div>
   );
 };
