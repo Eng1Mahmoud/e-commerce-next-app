55d9d4acd20974cc3b0d81a12214bea8657e1540
Mahmoud Mohamed <eslam.mohamed@fci.sohag.edu.eg>
2024-05-18 19:16:08 +0300

    last update

--- src/app/(main-layout)/page.tsx
+++ src/app/(main-layout)/page.tsx
@@ -1,12 +1,8 @@
 import { Categories } from "@/components/Home/categories/Categories";
 import { Slider } from "@/components/Home/slider/Slider";
 import { CategoriesSection } from "@/components/Home/categories-section/CategoriesSection";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/nextAuth";
-
 export default async function Home() {
-  const session = await getServerSession(authOptions)
-/*   console.log(session) */
+
   return (
     <div>
      <Slider/>
