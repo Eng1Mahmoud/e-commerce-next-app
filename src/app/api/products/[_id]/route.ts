a463a6094ef451553f39c3ec7874f3dc1b09a90d
Mahmoud Mohamed <eslam.mohamed@fci.sohag.edu.eg>
2024-05-18 14:11:34 +0300

    edit get product by id

--- src/app/api/products/[_id]/route.ts
+++ src/app/api/products/[_id]/route.ts
@@ -1,6 +1,6 @@
-export const GET = async ({ params }: { params: {_id:string} }) => {
-    const { _id } = params;
-    console.log("_id: ", _id);
+export const GET = async (req:any) => {
+    const { _id } = req.params;
+    console.log(_id);
 
     return Response.json({ message: "Product not found" }, { status: 404 });
 
