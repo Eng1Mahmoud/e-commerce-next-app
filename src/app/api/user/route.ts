57db01341ce047bf1a66ba308f76a8e41330e3fd
Mahmoud Mohamed <eslam.mohamed@fci.sohag.edu.eg>
2024-05-25 22:45:06 +0300

    delete user route

--- src/app/api/user/route.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import { User } from "@/lib/models/user";
-import { connectDb } from "@/lib/conectDb";
-import { verifyToken } from "@/lib/auth-helper/jwt";
-export const GET = async (req:any) => {
-
-
-};
