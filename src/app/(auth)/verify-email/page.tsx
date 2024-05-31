e20d1f807a8d4e09207f6e551df8f68233a162b2
Mahmoud Mohamed <eslam.mohamed@fci.sohag.edu.eg>
2024-05-31 19:42:04 +0300

    wrap VerifyEmailContent in Suspense becouse using useSearchParams

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -2,14 +2,15 @@
 import axiosInstance from "@/lib/axiosInstance";
 import { alertStore } from "@/store/alert";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 
-export default function VerifyEmail() {
+function VerifyEmailContent() {
   const [success, setSuccess] = useState("");
   const { setAlert } = alertStore();
   let router = useRouter();
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
+
   useEffect(() => {
     if (token) {
       axiosInstance
@@ -28,37 +29,43 @@ export default function VerifyEmail() {
   }, [router, setAlert, token]);
 
   return (
+    <div>
+      <h1 className="font-bold font-main text-[25px]">
+        {success === "verifyed" && "تم تفعيل الحساب بنجاح"}
+        {success === "failed" && "لم يتم تفعيل الحساب ربما انتهت صلاحية الرابط"}
+      </h1>
+      {success === "failed" && (
+        <button
+          className="btn btn-primary mt-2"
+          onClick={() => router.push("/register")}
+        >
+          انشاء حساب
+        </button>
+      )}
+      {success === "verifyed" && (
+        <button
+          className="btn btn-primary mt-2"
+          onClick={() => router.push("/login")}
+        >
+          {" "}
+          تسجيل الدخول
+        </button>
+      )}
+      {!success && (
+        <p className="font-bold font-main text-primary text-[25px]">
+          الرجاء الانتظار بضع ثواني يتم الان التحقق من حالة الحساب
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default function VerifyEmail() {
+  return (
     <div className="flex justify-center items-center h-[100vh]">
-      <div>
-        <h1 className="font-bold font-main text-[25px]">
-          {success === "verifyed" && "تم تفعيل الحساب بنجاح"}
-          {success === "failed" &&
-            "لم يتم تفعيل الحساب ربما انتهت صلاحية الرابط"}
-        </h1>
-        {success === "failed" && (
-          <button
-            className="btn btn-primary mt-2"
-            onClick={() => router.push("/register")}
-          >
-            انشاء حساب
-          </button>
-        )}
-        {success === "verifyed" && (
-          <button
-            className="btn btn-primary mt-2"
-            onClick={() => router.push("/login")}
-          >
-            {" "}
-            تسجيل الدخول
-          </button>
-        )}
-        {
-          !success &&
-          <p className="font-bold font-main text-primary text-[25px]">
-            الرجاء الانتظار بضع ثواني يتم الان التحقق من حالة الحساب
-          </p>
-        }
-      </div>
+      <Suspense fallback={<p>Loading...</p>}>
+        <VerifyEmailContent />
+      </Suspense>
     </div>
   );
 }
